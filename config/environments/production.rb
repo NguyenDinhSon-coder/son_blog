77583765e91cf8c6d62f4256b38150be1a9e8812
Nguyen Dinh Son <son261197@gmail.com>
2020-05-20 14:05:21 +0700

    Heybaee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,20 +7,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address
-    => 'smtp.sendgrid.net',
-    :port
-    => '587',
-    :authentication => :plain,
-    :user_name
-    => ENV['SENDGRID_USERNAME'],
-    :password
-    => ENV['SENDGRID_PASSWORD'],
-    :domain
-    => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => '587',
+  #   :authentication => :plain,
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
 
   config.cache_classes = true
 
@@ -110,7 +105,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Store uploaded files on Amazon AWS.
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
